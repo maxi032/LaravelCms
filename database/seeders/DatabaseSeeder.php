35e90d1bab4f937df3bff79449026ffa1f355abc
maxi032 <maxxyro@yahoo.com>
2023-09-19 14:26:40 +0200

    Added Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,11 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\CategoryTypesTableSeeder;
+use Database\Seeders\CategoryTableSeeder;
+use Database\Seeders\PostTypesTableSeeder;
+use Database\Seeders\PostTableSeeder;
+use Database\Seeders\PostTranslationsTableSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +17,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            CategoryTypesTableSeeder::class,
+            CategoryTableSeeder::class,
+            PostTypesTableSeeder::class,
+            PostTableSeeder::class,
+            PostTranslationsTableSeeder::class,
+        ]);
     }
 }
